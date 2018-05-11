669f70717e8dc1c057940f9ec1bc2d2145299f07
Ruaridh <ruaridhdunbar@gmail.com>
2018-05-11 11:16:35 +0100

    adds Drums class and passing test for getting number of pieces from drumkit

--- src/main/java/instruments/Drums.java
+++ src/main/java/instruments/Drums.java
@@ -1,4 +1,15 @@
 package instruments;
 
-public class Drums {
+public class Drums extends Instrument {
+
+    int numberOfPieces;
+
+    public Drums(String description, double buyPrice, double sellPrice, String type, String colour, int numberOfPieces) {
+        super(description, buyPrice, sellPrice, type, colour);
+        this.numberOfPieces = numberOfPieces;
+    }
+
+    public int getNumberOfPieces() {
+        return numberOfPieces;
+    }
 }
---
+++ src/test/java/DrumTest.java
@@ -0,0 +1,22 @@
+import instruments.Drums;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DrumTest {
+
+    Drums drums;
+
+    @Before
+    public void before(){
+        drums = new Drums("Pearl", 250, 340, "Acoustic", "Blue", 3);
+
+    }
+
+    @Test
+    public void canGetNumberOfPieces(){
+        assertEquals(3, drums.getNumberOfPieces());
+    }
+
+}
